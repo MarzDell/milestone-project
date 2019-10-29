bc925f1e1e806af0a5ba7e1dd6ff2eaa778504dd
MarzDell <chodnickamarzena@yahoo.co.uk>
2019-10-29 12:34:46 +0000

    Connected index.html with main.html and all working

--- app.py
+++ app.py
@@ -5,10 +5,17 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+
+app.config['MONGO_DBNAME'] = 'mileston-project'
+app.config['MONGO_URI'] = 'mongodb+srv://Cat:Kot@myfirstcluster-tlipu.mongodb.net/milestone-project?retryWrites=true&w=majority'
 mongo = PyMongo(app)
 
 @app.route('/')
 
+@app.route('/main')
+def main():
+    return render_template('main.html', 
+                            tasks=mongo.db.tasks.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
